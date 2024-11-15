c1d9ab9ad34a9c56f2e5ea7dcb1451efd6f67bc9
Vencyderry <vencyderry@gmail.com>
2024-11-15 20:21:09 +1000

    fix: deleted tool

--- tools.py
+++ tools.py
@@ -89,7 +89,7 @@ async def delete_mess(chat_id):
                 messages.remove(messages[0])
             ctx.set(f"messages_{chat_id}", messages)
 
-    ctx.delete(f"messages_{chat_id}")
+        ctx.delete(f"messages_{chat_id}")
 
 
 class Swear:
